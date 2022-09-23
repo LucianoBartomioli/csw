e86aadfd6992efaf389ef1065f7867ee2a20ee73
LucianoBartomioli <69654179+LucianoBartomioli@users.noreply.github.com>
2022-09-22 21:22:57 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import Presentacion from '../components/Presentacion'
 import CardTemas from '../components/CardTemas'
 
